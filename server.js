c79d60833842c5010180cf95506eeab9bffc01d2
ZiadFehr <ziad.shaker0@gmail.com>
2022-12-08 20:26:44 +0200

    commit on master

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-var http = require('http'),
-    fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+    res.setHeader('Content-Type', 'text/html');
+    let path = './html/';
+    switch (req.url) {
+        case '/':
+            path += 'home.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+        default:
+            path += 'err.html';
+            res.statusCode = 404;
+            break;
+    }
+    
+    fs.readFile(path, (err, data) => {
+        if (err) throw err;
+        //res.write();
+        res.end(data);
+    })
+});
+server.listen(3000, 'localhost', () => {
+    console.log('Listening')
 });
